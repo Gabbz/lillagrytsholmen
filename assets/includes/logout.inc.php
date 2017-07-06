7803b7113918e1b608094e2c63b83d4644b5add7
Jonas Borg <jonas.borg@netgain.se>
2017-07-06 09:59:12 +0200

    added redirect on logout

--- assets/includes/logout.inc.php
+++ assets/includes/logout.inc.php
@@ -3,25 +3,25 @@
     //session_save_path('tmp/');
     session_start();
 
-    //if (isset($_SESSION['username'])) {
+    if (isset($_SESSION['username'])) {
 
         
         $_SESSION = array();
 
-        if (ini_get("session.use_cookies")) {
+       /* if (ini_get("session.use_cookies")) {
             $params = session_get_cookie_params();
             setcookie(session_name(), '', time() - 42000,
                 $params["path"], $params["domain"],
                 $params["secure"], $params["httponly"]
             );
-        }
+        } */
 
         // Förstör sessionen
         session_destroy()
 
 
-    //}
+    }
     
-    //header("Location: /lillagrytsholmen.se/"); /* Redirect browser */
-    //exit();
+    header("Location: /lillagrytsholmen.se/"); /* Redirect browser */
+    exit();
 ?>
