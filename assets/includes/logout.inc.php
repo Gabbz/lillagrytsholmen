335747b7eff18e04ce2edc0337c1cc6b848f36cd
Jonas Borg <jonas.borg@netgain.se>
2017-07-06 09:28:43 +0200

    added redirect on logout

--- assets/includes/logout.inc.php
+++ assets/includes/logout.inc.php
@@ -1,12 +1,18 @@
 <?php
 
-    //if (isset($_SESSION['username'])) {
+    session_save_path('tmp/');
+    session_start();
+
+    if (isset($_SESSION['username'])) {
         // Tar bort alla sessionsvariabler
         session_unset();
 
         // Förstör sessionen
         session_destroy();
-    //}
+
+        $_SESSION = [];
+
+    }
     
     header("Location: /lillagrytsholmen.se/"); /* Redirect browser */
     exit();
