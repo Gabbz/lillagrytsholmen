1c6293837b99c149e89443242921ce475e1df892
Jonas <jonasaborg@gmail.com>
2017-08-03 21:03:22 +0200

    added picturebutton to photos

--- index.php
+++ index.php
@@ -270,9 +270,8 @@ include 'assets/includes/settings.inc.php';
 
 						<!-- photos -->
 							<article id="photos">
-								<h2 class="major">Fotoalbum</h2>
-								<span class="image main"><img src="images/pic03.jpg" alt="" /></span>
-								<p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
+								<h2 class="major">Fotoalbum</h2>	
+								<button type="submit" name="beer" value="beer_btn_was_clicked"><img src="images/pic02.jpg" /></button>
 							</article>
 
 						<!-- book -->
