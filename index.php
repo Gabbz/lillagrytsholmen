f582bc202f49a03ec6f8666319b429ad1c9464d7
Jonas Borg <jonas.borg@netgain.se>
2017-08-08 12:57:23 +0200

    added upload include sever script

--- index.php
+++ index.php
@@ -630,7 +630,7 @@ include 'assets/includes/settings.inc.php';
 			<script>
 				$('#file-sv').fileinput({
 					language: 'sv',
-					uploadUrl: 'assets/includes/upload.php',
+					uploadUrl: 'assets/includes/upload.inc.php',
 					allowedFileExtensions: ['jpg', 'png', 'gif'],
 					showZoom: false,
 					maxFileSize: 5000,
