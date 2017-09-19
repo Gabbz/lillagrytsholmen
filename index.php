247558e4f8cf58bbfaeb75a3ce943dd060c6dd97
Jonas Borg <jonas.borg@netgain.se>
2017-09-19 08:39:22 +0200

    asd

--- index.php
+++ index.php
@@ -390,7 +390,7 @@ include 'assets/includes/settings.inc.php';
 
 														});
 
-														$('.calendar').pignoseCalendar({
+														setTimeout(function(){$('.calendar').pignoseCalendar({
 															theme: 'dark',
 															lang: 'sv',
 															week: 1,
@@ -406,7 +406,7 @@ include 'assets/includes/settings.inc.php';
 															},
 															schedules: fillArray
 															//Lägg till scheduler som dynamiskt hämtar vilka datum som är redan upptagna
-														});
+														})}, 250);
 													});
 												</script>
 											</div>
