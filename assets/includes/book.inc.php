6ae1c46c977d8aa5a855dada911338cbb488eab6
Jonas Borg <jonas.borg@netgain.se>
2018-02-03 15:56:41 +0100

    asd

--- assets/includes/book.inc.php
+++ assets/includes/book.inc.php
@@ -9,11 +9,16 @@
     $resultArr = array();
 
 
-    var $i = 0;
+    $booking = new stdClass(); 
     while($row = mysqli_fetch_array($result)) {
-        $resultArr[] = $row;
-        $i++;
-        echo $i;
+        $booking->renter =  $row['renter'];
+        $booking->from_date_year =  substr($row['from_date'], 0, 4);
+        $booking->from_date_month =  substr($row['from_date'], 5, 2);
+        $booking->from_date_day =  substr($row['from_date'], 8, 2);
+        $booking->to_date_year = substr($row['to_date'], 0, 4);
+        $booking->to_date_month = substr($row['to_date'], 5, 2);
+        $booking->to_date_day = substr($row['to_date'], 8, 2);
+        $resultArr[] = $booking;
     }
 
 /*    
