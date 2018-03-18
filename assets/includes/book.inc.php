a483227d8522bb217d06822d15ce271cc139b7b7
Jonas Borg <jonasaborg@gmail.com>
2018-03-19 00:28:16 +0100

    asd

--- assets/includes/book.inc.php
+++ assets/includes/book.inc.php
@@ -27,7 +27,7 @@
         $book_name = htmlspecialchars(trim($_POST['book_name']));
         $book_name = str_replace(" ","_",$book_name);
         $book_message = htmlspecialchars(trim($_POST['book_message']));
-        echo 'Dates ' . $book_dates . ' To dates ' . $to_date . ' From date ' .  $from_date . ' Book name ' . $book_name . ' Message ' . $book_message
+        echo 'Dates ' . $book_dates . ' To dates ' . $to_date . ' From date ' .  $from_date . ' Book name ' . $book_name . ' Message ' . $book_message;
        
         // Query som skapar en bokning
         $query = "INSERT INTO booking VALUE(?,?,?,?)";
