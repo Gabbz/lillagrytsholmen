9f00f3c97caa967d102a0f6fa9f9648cef05539d
Jonas Borg <jonasaborg@gmail.com>
2018-03-19 00:14:21 +0100

    ...

--- assets/includes/book.inc.php
+++ assets/includes/book.inc.php
@@ -19,7 +19,7 @@
 
 
 
-
+    
     if (isset($_POST['book_submit']) && $_SESSION['privilege'] == 0) {
         $book_dates = htmlspecialchars(trim($_POST['book_dates']));
         $from_date = substr($book_dates, 0, -19);
@@ -28,7 +28,6 @@
         $book_message = htmlspecialchars(trim($_POST['book_message']));
        
         // Query som skapar en bokning
-    
         $query = "INSERT INTO booking VALUE(?,?,?,?)";
         if ($stmt = $mysqli->prepare($query)) {
             $stmt->bind_param("ssss", $book_name, $from_date, $to_date, $book_message);
@@ -44,15 +43,5 @@
     } elseif (isset($_POST['book_submit']) && $_SESSION['privilege'] == 0 && $_SESSION['username']) {
         $feedback = "Du måste logga in för att göra en bokning.";
     }
-
-?>
-
-
-
-
-
-
-
-
 
 ?>
