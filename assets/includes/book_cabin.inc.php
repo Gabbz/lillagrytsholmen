a5025bc67ad8efea59142bac34a4c2c1689df11a
Jonas Borg <jonasaborg@gmail.com>
2018-03-20 00:44:42 +0100

    asd

--- assets/includes/book_cabin.inc.php
+++ assets/includes/book_cabin.inc.php
@@ -7,14 +7,14 @@
         $book_name_replace = str_replace(" ","_",$book_name);
         $book_message = htmlspecialchars(trim($_POST['book_message']));
         debug_to_console( "funkar");
-        debug_to_console( "book_name: " . $book_name_replace . " book_message: " . $book_message . " from_date: " . $from_date . " to_date: " . $to_date);
+        debug_to_console( "book_name: " . $book_name_replace  . " from_date:" . $from_date . "to_date:" . $to_date. "book_message: " . $book_message);
        
         // Query som skapar en bokning
         $query = "INSERT INTO booking VALUE(?,?,?,?)";
         debug_to_console( "funkar2");
         if ($stmt = $mysqli->prepare($query)) {
             debug_to_console( "funkar3");
-            $stmt->bind_param("ssss", $book_name_replace, $from_date, $to_date, $book_message);
+            $stmt->bind_param("siis", $book_name_replace, $from_date, $to_date, $book_message);
             if ($stmt->execute()) {
                 debug_to_console( "funkar4");
                 $feedback = "Bokningen för "  . $book_name . " lyckades!";
