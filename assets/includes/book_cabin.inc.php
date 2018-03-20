247e93c4efc88f428b68c99a84b46c04f8a50321
Jonas Borg <jonasaborg@gmail.com>
2018-03-20 15:45:45 +0100

    asd

--- assets/includes/book_cabin.inc.php
+++ assets/includes/book_cabin.inc.php
@@ -9,8 +9,8 @@
         $book_name = htmlspecialchars(trim($_POST['book_name']));
         $book_name_replace = str_replace(" ","_",$book_name);
         $book_message = htmlspecialchars(trim($_POST['book_message']));
-        debug_to_console( "funkar");
-        debug_to_console( "book_name: " . $book_name_replace . " from_date:" . $from_date . "to_date:" . $to_date. "book_message: " . $book_message);
+        //debug_to_console( "funkar");
+        //debug_to_console( "book_name: " . $book_name_replace . " from_date:" . $from_date . "to_date:" . $to_date. "book_message: " . $book_message);
        
         // Query som skapar en bokning
         //$query = "INSERT INTO booking VALUE(?,?,?,?)";
