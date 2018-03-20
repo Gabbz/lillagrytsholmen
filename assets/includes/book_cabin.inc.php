8a051b73e4097e8622bc7bea39ab4a91fca1a53c
Jonas Borg <jonasaborg@gmail.com>
2018-03-20 21:58:21 +0100

    asd

--- assets/includes/book_cabin.inc.php
+++ assets/includes/book_cabin.inc.php
@@ -3,6 +3,7 @@
     include('db_connect.inc.php');
 
     if (isset($_POST['book_submit']) && $_SESSION['privilege'] == 0) {
+        
         $book_dates = htmlspecialchars(trim($_POST['book_dates']));
         $from_date = date(substr($book_dates, 0, -19) . ":00");
         $to_date = date(substr($book_dates, 19) . ":00");
@@ -10,6 +11,19 @@
         $book_name_replace = str_replace(" ","_",$book_name);
         $book_message = htmlspecialchars(trim($_POST['book_message']));
        
+        mysqli_select_db($mysqli, "lillagrytsholmen.se");
+        $query = "SELECT * FROM booking";
+        $result = mysqli_query($mysqli,$query);
+
+        $resultArr = array();
+
+        $booking = new stdClass(); 
+        while($row = mysqli_fetch_array($result)) {
+            $resultArr[] = $row;
+        }
+
+        
+
         // Query som skapar en bokning
         $query = "INSERT INTO booking VALUES (NULL,?,CURRENT_TIMESTAMP,?,?,?)";
         //$stmt = $mysqli->prepare("INSERT INTO booking VALUES (?,?,?,?,?,?)");
@@ -23,30 +37,7 @@
                 $feedback = "Något med bokningen gick fel. Var vänlig kontakta systemadministratör.";
             }
             $stmt->close();
-            
         }
-
-        //$stmt = $mysqli->prepare("INSERT INTO booking VALUES (NULL, ?,CURRENT_TIMESTAMP,?,?,?)");
-        // prepare() can fail because of syntax errors, missing privileges, ....
-        //if ( false===$stmt ) {
-        // and since all the following operations need a valid/ready statement object
-        // it doesn't make sense to go on
-        // you might want to use a more sophisticated mechanism than die()
-        // but's it's only an example
-        //die('prepare() failed: ' . htmlspecialchars($mysqli->error));
-        //}        $rc = $stmt->bind_param('ssss', $book_name_replace, $from_date, $to_date, $book_message);
-        // bind_param() can fail because the number of parameter doesn't match the placeholders in the statement
-        // or there's a type conflict(?), or ....
-        //if ( false===$rc ) {
-        // again execute() is useless if you can't bind the parameters. Bail out somehow.
-        //die('bind_param() failed: ' . htmlspecialchars($stmt->error));
-        //} $rc = $stmt->execute();
-        // execute() can fail for various reasons. And may it be as stupid as someone tripping over the network cable
-        // 2006 "server gone away" is always an option
-        /*if ( false===$rc ) {
-        die('execute() failed: ' . htmlspecialchars($stmt->error));
-        }*/
-
         $stmt->close();
     
     } elseif (isset($_POST['book_submit']) && $_SESSION['privilege'] == 0 && $_SESSION['username']) {
