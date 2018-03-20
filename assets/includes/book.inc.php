674822397dc32c25eb8eaa8e21259deceb052b78
Jonas Borg <jonasaborg@gmail.com>
2018-03-20 13:39:03 +0100

    gabbz

--- assets/includes/book.inc.php
+++ assets/includes/book.inc.php
@@ -2,15 +2,19 @@
 
     include("db_connect.inc.php");
 
-    mysqli_select_db($mysqli);
+    mysqli_select_db($mysqli, "lillagrytsholmen.se");
+    debug_to_console("kommer hit 1");
     $query = "SELECT * FROM booking";
+    debug_to_console("kommer hit 2");
     $result = mysqli_query($mysqli,$query);
+    debug_to_console("kommer hit 3");
 
     $resultArr = array();
 
 
     $booking = new stdClass(); 
     while($row = mysqli_fetch_array($result)) {
+        debug_to_console("kommer hit " . $row);
         $resultArr[] = $row;
     }
 
