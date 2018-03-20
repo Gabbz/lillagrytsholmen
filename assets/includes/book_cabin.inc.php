eab02959a4d259bc418afb51168db9ca3e2f8771
Jonas Borg <jonasaborg@gmail.com>
2018-03-20 23:16:00 +0100

    asd

--- assets/includes/book_cabin.inc.php
+++ assets/includes/book_cabin.inc.php
@@ -24,6 +24,16 @@
             $resultArr[] = $row;
         }
 
+        function check_in_range($start_date, $end_date, $date_from_user) {
+            // Convert to timestamp
+            $start_ts = strtotime($start_date);
+            $end_ts = strtotime($end_date);
+            $user_ts = strtotime($date_from_user);
+
+            // Check that user date is between start & end
+            return (($user_ts >= $start_ts) && ($user_ts <= $end_ts));
+        }
+
         $checker = 0;
         foreach ($resultArr as &$value) {
             $checker = check_in_range($value[3], $value[4], $from_date);
