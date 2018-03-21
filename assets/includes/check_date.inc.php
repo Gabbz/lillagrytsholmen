2a510f94edb4cc00c95d3fe6f0268f394fbdc5ff
Jonas Borg <jonasaborg@gmail.com>
2018-03-21 18:58:29 +0100

    asd

--- assets/includes/check_date.inc.php
+++ assets/includes/check_date.inc.php
@@ -3,6 +3,9 @@
     function check_in_range($start_date, $end_date, $date_from_user)
     {
     // Convert to timestamp
+    $start_ts = $start_date;
+    $end_ts = $end_date;
+    $user_ts = $date_from_user;
 
     debug_to_console("Start: " . $start_date . " End: " . "$end_date" . " User: " . $date_from_user);
 
