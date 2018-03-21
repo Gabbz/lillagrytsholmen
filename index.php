edc8c929f7c9b5461fbaaadf729c1c8f326bcafc
Jonas Borg <jonasaborg@gmail.com>
2018-03-21 16:48:53 +0100

    added favicon

--- index.php
+++ index.php
@@ -20,6 +20,8 @@ include 'assets/includes/book_cabin.inc.php';
 		<title>Lilla Grytholmen</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="images/favicon.ico" type="image/x-icon">
 
 		<!-- CSS -->
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
