ea4018e7017e745ad172fbe97c9fa9b68437b936
Jonas Borg <jonasaborg@gmail.com>
2018-03-21 18:42:43 +0100

    asd

--- assets/includes/check_date.inc.php
+++ assets/includes/check_date.inc.php
@@ -3,14 +3,11 @@
     function check_in_range($start_date, $end_date, $date_from_user)
     {
     // Convert to timestamp
-    $start_ts = $start_date;
-    $end_ts = $end_date;
-    $user_ts = $date_from_user;
 
-    debug_to_console("Start: " . $start_ts . " End: " . "$end_ts" . " User: " . $user_ts);
+    debug_to_console("Start: " . $start_date . " End: " . "$end_date" . " User: " . $date_from_user);
 
     // Check that user date is between start & end
-    if (($user_ts >= $start_ts) && ($user_ts <= $end_ts)) return 1;
+    if (($date_from_user >= $start_date) && ($date_from_user <= $end_date)) return 1;
     else return 0;
     }
 
