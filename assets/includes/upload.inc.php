b8ea702a72970f8629d427b1dc58eac5563d7f37
Jonas Borg <jonasaborg@gmail.com>
2018-04-18 20:24:42 +0200

    asd

--- assets/includes/upload.inc.php
+++ assets/includes/upload.inc.php
@@ -27,6 +27,7 @@
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
     $uploadOk = 1;
     $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    debug_to_console($imageFileType);
     // Check if image file is a actual image or fake image
     if(isset($_POST["submit"])) {
         $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
