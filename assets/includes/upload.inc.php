b74d6548164789470a1c764be303f916806c0c9d
Jonas Borg <jonasaborg@gmail.com>
2018-04-23 21:11:50 +0200

    asd

--- assets/includes/upload.inc.php
+++ assets/includes/upload.inc.php
@@ -15,63 +15,66 @@
     debug_to_console("Körs");
 
 
-
-
-
-    debug_to_console(basename($_FILES["files"]));
-
-
-
-    $target_dir = "../../uploads/testingz";
-    $target_file = $target_dir . basename($_FILES["files"]);
-    $uploadOk = 1;
-    $imageFileType = pathinfo($target_file);
-    debug_to_console(pathinfo($imageFileType));
-    debug_to_console($imageFileType);
-    // Check if image file is a actual image or fake image
-    if(isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-        if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-        } else {
-            echo "File is not an image.";
-            $uploadOk = 0;
+    // upload.php de http://webtips.krajee.com/ajax-based-file-uploads-using-fileinput-plugin/
+    if (empty($_FILES['files'])) {
+        return; // or process or throw an exception
+    }
+    
+    // get the files posted
+    $ticket = $_FILES['files'];
+    
+    // get server posted
+    $server = empty($_POST['server']) ? '' : $_POST['server'];
+    
+    // get user name posted
+    $user = empty($_POST['user']) ? '' : $_POST['user'];
+    
+    // a flag to see if everything is ok
+    $success = null;
+    
+    // file paths to store
+    $paths= [];
+    
+    // loop and process files
+    for($i=0; $i < count($ticket); $i++){
+        echo $i;
+        $ext = explode('.', basename($ticket['name'][$i]));
+        $target = "uploads" . DIRECTORY_SEPARATOR . md5(uniqid()) . "." . array_pop($ext);
+        if(move_uploaded_file($ticket['tmp_name'][$i], $target)) {
+            $success = true;
+            $paths[] = $target;
+            debug_to_console("fan det gick fan");
+        } else{
+            $success = false;
+            break;
         }
     }
-
-    // Check if file already exists
-    if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
-        $uploadOk = 0;
-    }
-    // Check file size
-    if ($_FILES["fileToUpload"]["size"] > 5000000) {
-        echo "Sorry, your file is too large.";
-        $uploadOk = 0;
-    }
-    // Allow certain file formats
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg                                                                                                                                                                                                                                             "
-    && $imageFileType != "gif" && $imageFileType != "JPG") {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        $uploadOk = 0;
-    }
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
-    // if everything is ok, try to upload file
-    } else {
-        for ($i=0; $i < 1; $i++) {
-            $ext = explode('.', basename($_FILES["fileToUpload"]["name"]));
-            $target = "uploads" . DIRECTORY_SEPARATOR . md5(uniqid()) . "." . array_pop($ext);                                                                                                                                                                                                                                             pop($ext);
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file                                                                                                                                                                                                                                             )) {
-                echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has                                                                                                                                                                                                                                              been uploaded.";
-            } else {
-                echo "Sorry, there was an error uploading your file.";
-            }
-        }
-    }
-?>
+    
+    // check and process based on successful status
+    //if ($success === true) {
+        // call the function to save all data to database
+        // code for the following function `save_data` is not
+        // mentioned in this example
+      //  save_data($userid, $username, $paths);
+    
+        // store a successful response (default at least an empty array). You
+        // could return any additional response info you need to the plugin for
+        // advanced implementations.
+        //$output = [];
+    //} elseif ($success === false) {
+      //  $output = ['error'=>'Error while uploading ticket. Contact the system administrator'];
+        // delete any uploaded files
+        //foreach ($paths as $file) {
+      //      unlink($file);
+        //}
+    /*} else {
+        $output = ['error'=>'No files were processed.'];
+    }*/
+    
+    // return a json encoded response for plugin to process successfully
+    //echo json_encode($output);
+    ?>
+    
 
 
 
