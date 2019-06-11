dd3185e9b8118a11a39c44a405b34c38ccf87ff6
Jonas Borg <jonasaborg@gmail.com>
2019-06-11 16:43:25 +0200

    asd

--- index.php
+++ index.php
@@ -261,7 +261,7 @@ include 'assets/includes/book_cabin.inc.php';
 											<td>
 												<label style="opacity: 0;">spacer</label>
 												<ul class="actions" style="float: right; margin: 0;">
-													<li><input type="button" id="settings_submit" name="settings_submit" onclick"submitSettings();" value="Verkställ" /></li>
+													<li><input type="button" id="settings_submit" name="settings_submit" onclick="submitSettings();" value="Verkställ" /></li>
 													<!--<li><button type="button" id="settings_submit" name="settings_submit" onclick"submitSettings();">Verkställ</button></li>-->
 												</ul>
 											</td>
