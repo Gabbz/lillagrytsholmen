7ffc7a958f0170a9bb9e5fa65f139b780a179b68
Jonas Borg <jonasaborg@gmail.com>
2019-06-12 15:11:44 +0200

    asd

--- assets/includes/login.inc.php
+++ assets/includes/login.inc.php
@@ -39,8 +39,8 @@
         } 
     }
 
-    echo "{feedback: " . $feedback . ",";
-    echo "fullName: " . $_SESSION['fullname'] . ",";
-    echo "status: 0}";
+    echo "'{\"feedback\": " . $feedback . ",";
+    echo "\"fullName\": " . $_SESSION['fullname'] . ",";
+    echo "\"status\": \"0\"}'";
     
 ?>
