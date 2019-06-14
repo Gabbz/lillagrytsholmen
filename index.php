feb3b78ed3d3ceeca501d3fecfdd358519038f43
Jonas Borg <jonasaborg@gmail.com>
2019-06-14 17:38:23 +0200

    asd

--- index.php
+++ index.php
@@ -766,7 +766,7 @@ include 'assets/includes/book_cabin.inc.php';
 
 				if (sessionUserName == "no_session" && isLoggedOut == "true") {
 					triggerSnackbar("Du är nu utloggad.");
-					<?php $_SESSION = []; ?>
+					
 				}
 			</script>
 
